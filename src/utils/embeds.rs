d2daca28bf4dfcbbb1d8521be5a4d03c5390957b
Nathan Miguel <nathan.cm77@gmail.com>
2025-03-23 21:31:53 -0300

    Optimized imports

--- src/utils/embeds.rs
+++ src/utils/embeds.rs
@@ -1,6 +1,5 @@
 use twilight_model::channel::message::Embed;
 use twilight_util::builder::embed;
-use crate::settings::global::Colors;
 
 pub fn res(color: u32, content: String) -> Embed {
     embed::EmbedBuilder::new()
