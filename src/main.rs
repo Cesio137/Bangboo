df0dc6e64ef3409e6461bd31f0cf9fd666a865bd
Nathan Miguel <nathan.cm77@gmail.com>
2025-04-25 14:15:22 -0300

    Fixed intents

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,23 @@ mod settings;
 mod tools;
 mod utils;
 
-
+#[cfg(target_env = "gnu")]
+use utils::malloc::malloc::configure_malloc;
 use std::sync::Arc;
 use discord::app::base::App;
 use settings::env::ENV_SCHEMA;
 use serenity::prelude::*;
 
 #[tokio::main]
-async fn main() {    
+async fn main() { 
+    #[cfg(target_env = "gnu")]
+    configure_malloc();
+
     let intents = GatewayIntents::GUILD_MESSAGES
         | GatewayIntents::DIRECT_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+        | GatewayIntents::MESSAGE_CONTENT
+        | GatewayIntents::GUILD_MEMBERS
+        | GatewayIntents::GUILD_MODERATION;
 
     let app = Arc::new(App::new());
     let mut client = Client::builder(&ENV_SCHEMA.bot_token, intents)
