b7299f569a7329d4610e9a542e5d707593ba8e55
Nathan Miguel <nathan.cm77@gmail.com>
2025-05-09 10:37:39 -0300

    exposed gc

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,13 @@ import { bootstrap } from "#base";
 import { GlobalFonts } from "@napi-rs/canvas";
 import { join } from "node:path";
 
+if (global.gc) {
+    setInterval(
+        function() {
+            if (global.gc) global.gc();
+        },
+        300000
+    )
+}
 GlobalFonts.loadFontsFromDir(join(__rootname, "assets/fonts"));
 await bootstrap({ meta: import.meta });
