62139bd15aa193e32994f4695aa100b3789e2438
Nathan Miguel <nathan.cm77@gmail.com>
2025-07-01 11:29:59 -0300

    updated bot cards

--- src/discord/events/ready.ts
+++ src/discord/events/ready.ts
@@ -6,11 +6,9 @@ createEvent({
     name: "ready",
     event: "ready",
     async run(client) {
-        client.application.edit({
+        await client.application.edit({
             description: brBuilder(
-                "**Bangboo**",
-                "",
-                "Bangboo created by **Nathan Miguel** to watch his server!",
+                "Bangboo are small, rabbit-like robots. They were invented by the Void Hunter **Sunbringer**, Bangboo have since evolved to become **comprehensive and versatile intelligent individuals** that are indispensable helpers in New Eridu's daily operations. They are commonly seen in all parts of New Eridu.",
                 "",
                 "Source code: https://github.com/Cesio137/Bangboo"
             )
@@ -20,6 +18,7 @@ createEvent({
             { name: "Whatching Nathan's server!", type:ActivityType.Custom },
             { name: "Join in: .gg/DBNATxA6Jx", type:ActivityType.Custom },
             { name: "Zenless Zone Zero!", type:ActivityType.Custom },
+            { name: "I speak in grunts (textualized as **Ehn-ne's**)", type:ActivityType.Custom },
         ];
 
         let i = 0;
@@ -31,7 +30,7 @@ createEvent({
                 });
                 i++;
             },
-            20000
+            30_000
         );        
     },
 });
--- src/functions/utils/global.ts
+++ src/functions/utils/global.ts
@@ -13,68 +13,50 @@ import { channels, icon, roles } from "#functions";
 import { settings } from "#settings";
 
 export async function globalMessage( event: Events, member: GuildMember | PartialGuildMember | undefined, user: User, channel: TextChannel) {
-    const canvas = new Canvas(1024, 260);
+    const canvas = new Canvas(2800, 560);
     const context = canvas.getContext("2d");
-    const background = await loadImage(
-        join(
-            __rootname,
-            `assets/canvas/${
-                event === Events.GuildMemberAdd ? "join" : event === Events.GuildMemberRemove ? "leave" : "ban"
-            }.png`
-        )
-    );
-    context.drawImage(background, 0, 0, canvas.width, canvas.height);
-    context.save();
-
-    const avatar = await loadImage(user.displayAvatarURL({ size: 256 }));
-    context.beginPath();
-    context.arc(90 + 68, 90 + 41, 90, 0, Math.PI * 2);
-    context.clip();
-    context.drawImage(avatar, 68, 41, 180, 180);
-    context.restore();
-
-    const actionIconPath = join(
-        __rootname,
-        `assets/icons/static/Card/${
-            event === Events.GuildMemberAdd ? "add" : event === Events.GuildMemberRemove ? "minus" : "hammer"
-        }.png`
-    );
-    const actionIcon = await loadImage(actionIconPath);
-    context.drawImage(actionIcon, 205, 179);
-
+    
+    let bgpath = join(__rootname, "assets/canvas/");
     if (event === Events.GuildMemberAdd && typeof member !== "undefined") {
         const { joinedTimestamp } = member;
         const accountAge = Date.now() - user.createdTimestamp;
         const joinAge = Date.now() - (joinedTimestamp || 0);
         const timeLimit = 60 * 1000;
-        const welcomeText =
+        bgpath =
             joinAge < timeLimit && accountAge > timeLimit
-                ? "FIRST TIME"
-                : "WELCOME BACK";
-        context.fillStyle = "#FFFFFF";
-        context.font = "semibold 16px Poppins";
-        context.textBaseline = "middle";
-        const xPos = welcomeText === "FIRST TIME" ? 556 : 533;
-        context.fillText(welcomeText, xPos, 66 + 8);
+                ? join(bgpath, "card-new-member.png")
+                : join(bgpath, "card-back-to-server.png");
+    } else {
+        bgpath = join(bgpath, event === Events.GuildMemberRemove ? "card-left.png" : "card-mod.png");
     }
+    const background = await loadImage(bgpath);
+    context.drawImage(background, 0, 0, canvas.width, canvas.height);
+    context.save();
+
+    const avatar = await loadImage(user.displayAvatarURL({ size: 512 }));
+    context.beginPath();
+    context.arc(200, 200 + 160, 200, 0, Math.PI * 2);
+    context.clip();
+    context.drawImage(avatar, 0, 160, 400, 400);
+    context.restore();
 
     const { username, displayName } = user;
 
-    let usernameFontSize = 60;
+    let usernameFontSize = 200;
     context.fillStyle = "#FFFFFF";
     do {
-        context.font = `bold ${--usernameFontSize}px Rubik`;
+        context.font = `medium ${--usernameFontSize}px Fredoka`;
     } while (context.measureText(username).width > canvas.width - 400);
     context.textBaseline = "middle";
-    context.fillText(username, 300, 110 + usernameFontSize / 2);
+    context.fillText(username, 530, 140 + usernameFontSize / 2);
 
-    let displayNameFontSize = 32;
+    let displayNameFontSize = 96;
     context.fillStyle = "#FFFFFF";
     do {
-        context.font = `regular ${--displayNameFontSize}px Lato`;
+        context.font = `medium ${--displayNameFontSize}px Roboto`;
     } while (context.measureText(`@${displayName}`).width > canvas.width - 400);
     context.textBaseline = "middle";
-    context.fillText(`@${displayName}`, 300, 170 + displayNameFontSize / 2);
+    context.fillText(`@${displayName}`, 530, 380 + displayNameFontSize / 2);
 
     const buffer = await canvas.encode("png");
     const attachment = new AttachmentBuilder(buffer, { name: "card.png" });
