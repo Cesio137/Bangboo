717ef799a63d221d8e21134b99c8f82ec7fb37f2
Nathan Miguel <nathan.cm77@gmail.com>
2025-06-30 23:29:44 -0300

    added status

---
+++ src/discord/events/ready.ts
@@ -0,0 +1,37 @@
+import { createEvent } from "#base";
+import { brBuilder } from "@magicyan/discord";
+import { ActivitiesOptions, ActivityType } from "discord.js";
+
+createEvent({
+    name: "ready",
+    event: "ready",
+    async run(client) {
+        client.application.edit({
+            description: brBuilder(
+                "**Bangboo**",
+                "",
+                "Bangboo created by **Nathan Miguel** to watch his server!",
+                "",
+                "Source code: https://github.com/Cesio137/Bangboo"
+            )
+        });
+
+        const status: ActivitiesOptions[] = [
+            { name: "Whatching Nathan's server!", type:ActivityType.Playing },
+            { name: "Join in: .gg/DBNATxA6Jx", type:ActivityType.Playing },
+            { name: "Zenless Zone Zero!", type:ActivityType.Playing },
+        ];
+
+        let i = 0;
+        setInterval(
+            function() {
+                client.user.setPresence({
+                    status: "online",
+                    activities: [status[i % status.length]]
+                });
+                i++;
+            },
+            20_000
+        );        
+    },
+});
