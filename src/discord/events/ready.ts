099e2008537e064997b0dd69a3196d8d78c5a6dc
Nathan Miguel <nathan.cm77@gmail.com>
2025-08-04 16:39:36 -0300

    removed brbuilder for description

--- src/discord/events/ready.ts
+++ src/discord/events/ready.ts
@@ -1,24 +1,13 @@
 import { createEvent } from "#base";
-import { brBuilder } from "@magicyan/discord";
 import { ActivitiesOptions, ActivityType } from "discord.js";
 
 createEvent({
     name: "ready",
     event: "ready",
     async run(client) {
-        await client.application.edit({
-            description: brBuilder(
-                "Bangboo are small, rabbit-like robots. They were invented by the Void Hunter **Sunbringer**, Bangboo have since evolved to become **comprehensive and versatile intelligent individuals** that are indispensable helpers in New Eridu's daily operations. They are commonly seen in all parts of New Eridu.",
-                "",
-                "Source code: https://github.com/Cesio137/Bangboo"
-            )
-        });
-
         const status: ActivitiesOptions[] = [
-            { name: "Whatching Nathan's server!", type:ActivityType.Custom },
+            { name: "Powered by discloud", type:ActivityType.Custom },
             { name: "Join in: .gg/DBNATxA6Jx", type:ActivityType.Custom },
-            { name: "Zenless Zone Zero!", type:ActivityType.Custom },
-            { name: "I speak in grunts (textualized as **Ehn-ne's**)", type:ActivityType.Custom },
         ];
 
         let i = 0;
@@ -30,7 +19,7 @@ createEvent({
                 });
                 i++;
             },
-            30_000
+            15_000
         );        
     },
 });
