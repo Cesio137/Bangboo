8c1c3e9b5a801743d11cf101cc1a1631df0c6f73
Nathan Miguel <nathan.cm77@gmail.com>
2025-08-10 10:39:32 -0300

    Fixed guild info imports

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,6 +1,3 @@
 // export functions here
 export * from "./utils/embed.js"
-export * from "./utils/emoji.js"
-export * from "./utils/fab.js"
 export * from "./utils/global.js"
-export * from "./utils/guild.js"
