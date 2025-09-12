196f74edcb0c06e13c8c20fe8233a5bc18b61f9f
Nathan Miguel <nathan.cm77@gmail.com>
2025-09-12 10:29:40 -0300

    added missing macros for serenity-rs work properly

--- src/tools/discloud.rs
+++ src/tools/discloud.rs
@@ -3,7 +3,7 @@ use once_cell::sync::Lazy;
 use regex::Regex;
 use crate::env::ENV;
 
-pub const APPID: &str = "1757510952531";
+pub const APPID: &str = "1757608889661";
 
 pub static ASCII_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"\x1b\[[0-9;]*m").unwrap());
 
