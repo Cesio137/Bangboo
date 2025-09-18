695fe6ea20559275b08e1b8ba353240746fed5f6
Nathan Miguel <nathan.cm77@gmail.com>
2025-09-18 13:14:19 -0300

    updated package version

--- src/tools/discloud.ts
+++ src/tools/discloud.ts
@@ -1,5 +1,5 @@
 import { env } from "#env";
 import { discloud } from "discloud.app";
 
-export const appID = "1757684892025";
+export const appID = "1757689331873";
 export const user = await discloud.login(env.DISCLOUD_TOKEN);
