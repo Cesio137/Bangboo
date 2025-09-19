6ad1d015475840a1b7227af4b95f68fd9427bd7c
Nathan Miguel <nathan.cm77@gmail.com>
2025-09-19 14:36:34 -0300

    added function to read discloud.config

--- src/tools/discloud.ts
+++ src/tools/discloud.ts
@@ -1,5 +1,37 @@
 import { env } from "#env";
 import { discloud } from "discloud.app";
+import fs from "node:fs"
+import z from "zod";
 
-export const appID = "1757689331873";
+function parseConfigFile(content: string): Record<string, string> {
+    return Object.fromEntries(
+        content
+            .split('\n')
+            .map(line => line.trim())
+            .filter(line => line && !line.startsWith('#'))
+            .map(line => {
+                const [key, ...vals] = line.split('=');
+                return [key, vals.join('=').trim()];
+            })
+    );
+}
+
+function discloudAppID(): string {
+    const filePath = "./discloud.config";
+    const content = fs.readFileSync(filePath, "utf8");
+    const configObj = parseConfigFile(content);
+
+    const configSchema = z.object({
+        ID: z.string().optional()
+    });
+
+    const result = configSchema.safeParse(configObj);
+
+    console.log(result.data);
+
+    if (!result.success) return "";
+    return typeof result.data.ID !== "undefined" ? result.data.ID : "";
+}
+
+export const appID = discloudAppID();
 export const user = await discloud.login(env.DISCLOUD_TOKEN);
