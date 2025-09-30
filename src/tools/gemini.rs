c57d5f1008b98bcd7181860d79f4e2eae011b1a8
Nathan Miguel <nathan.cm77@gmail.com>
2025-09-30 11:46:12 -0300

    Updated genai to 0.4 and fixed some deprecates warn

--- src/tools/gemini.rs
+++ src/tools/gemini.rs
@@ -10,13 +10,12 @@ pub async fn get_text(prompt: String) -> anyhow::Result<String> {
     _ = GEMINI.resolve_service_target(MODEL_GEMINI).await?;
 
     let chat_req = ChatRequest::new(vec![
-        ChatMessage::system("Answer in one sentence"),
+        //ChatMessage::system(""),
         ChatMessage::user(prompt),
     ]);
 
     let chat_res = GEMINI.exec_chat(MODEL_GEMINI, chat_req, None).await?;
 
-    Ok(chat_res
-        .content_text_into_string()
-        .unwrap_or("NONE".to_string()))
+    let texts = &chat_res.into_texts();
+    Ok(texts.join("\n"))
 }
