343ad0ddfa1e1bb2113b411ef881f5b81cbb6ae2
Cesio137 <nathan.cm77@gmail.com>
2025-10-19 19:15:37 -0300

    Updated automod scam to prevent attachment link

--- src/tools/automod.ts
+++ src/tools/automod.ts
@@ -2,27 +2,31 @@ import { logger } from "#functions";
 import { createEmbed } from "@magicyan/discord";
 import { Message, OmitPartialGroupDMChannel } from "discord.js";
 
-const regex = new RegExp("\\[\\s*steam[^\\]]*]\\((https?:\\/\\/[^)]+)\\)", "gi");
+const regexSteam = new RegExp("\\[\\s*steam[^\\]]*]\\((https?:\\/\\/[^)]+)\\)", "gi");
+const attachmentPattern = '(https:\\/\\/media\\.discordapp\\.net\\/attachments\\/\\d+\\/\\d+\\/[^\\s]+\\.(?:png|jpg|jpeg|webp)(?:\\?[^\\s]*)?)';
 
 export async function filterMessage(
     message: OmitPartialGroupDMChannel<Message<boolean>>
 ) {
     const { author, content, guild, member } = message;
-    if (!regex.test(content)) return;
 
+    if (!guild) return;
+
+    const steamTest = regexSteam.test(content);
+
+    const attachmentMatch = content.match(attachmentPattern);
+    const attachmentTest = attachmentMatch ? attachmentMatch.length > 1 : false;
+
+    if (!steamTest && !attachmentTest) return;
+    
     const username = author.globalName || author.username;
-    let warning_message = `**${username}** sent a message that was flagged as a scam. Messages containing ***[text](hyperlink)*** are strictly prohibited. Bangboo (me) will presume his/her account has been compromised, leading to a server kick!`;
+    let warning_message = `**${username}** sent a message that was flagged as a scam. Messages containing **[steam...](hyperlink)** or **more than 1 image attachments** are strictly prohibited. Bangboo (me) will presume his/her account has been compromised, leading to a server kick!`;
     const embed = createEmbed({
         color: "Yellow",
         description: warning_message,
     });
     await message.reply({ embeds: [embed] });
     await message.delete();
-
-    if (!guild) {
-        logger.error("Failed to fetch guild.");
-        return;
-    }
 
     if (guild.ownerId === author.id) {
         logger.error("Tried to kick the owner of the guild");
@@ -34,11 +38,11 @@ export async function filterMessage(
         return;
     }
 
-    member.kick(`${username} sent a scam message!`);
+    await member.kick(`${username} sent a scam message!`);
     const embed_dm = createEmbed({
         color: "Yellow",
         description:
-            "It look like you probably got hacked and sent a message that was flagged as scam containing ***[text](hyperlink)***. You were just kicked from the server, but feel free to come back as soon as you resolve the issue with your account.",
+            "It look like you probably got hacked and sent a message that was flagged as scam containing **[steam...](hyperlink)** or **more than 1 image attachments**. You were just kicked from the server, but feel free to come back as soon as you resolve the issue with your account.",
     });
-    author.send({ embeds: [embed_dm] });
+    await author.send({ embeds: [embed_dm] });
 }
